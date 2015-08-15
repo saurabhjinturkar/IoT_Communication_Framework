69d2fe960d41ddf4bd9a63bb8988b8fd4daefba1
saurabhjinturkar <saurabhjinturkar@gmail.com>
2015-08-15 23:12:50 +0530

    Added Kafka Broker and RabbitMQBroker initial implementation

---
+++ src/core/internal/kafka/KafkaBroker.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package core.internal.kafka;
+
+import java.util.Properties;
+import java.util.concurrent.Future;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+import core.internal.AbstractBroker;
+
+/**
+ * @author Saurabh
+ *
+ */
+public class KafkaBroker extends AbstractBroker {
+	private static KafkaProducer<String, String> producer;
+
+	@Override
+	public void init() {
+		super.init();
+		Properties props = new Properties();
+		props.put("bootstrap.servers", "54.69.1.169:9092");
+		props.put("key.serializer",
+		    "org.apache.kafka.common.serialization.StringSerializer");
+		props.put("value.serializer",
+		    "org.apache.kafka.common.serialization.StringSerializer");
+		props.put("request.required.acks", "1");
+		producer = new KafkaProducer<String, String>(props);
+	}
+
+	@Override
+	public void close() {
+		super.close();
+		producer.close();
+	}
+
+	@Override
+	public void publish(String data, String topic) {
+		super.publish(data, topic);
+		ProducerRecord<String, String> datapacket = new ProducerRecord<String, String>(
+		    topic, "1", data);
+
+		Future<RecordMetadata> rs = producer.send(datapacket,
+		    new org.apache.kafka.clients.producer.Callback() {
+
+			    @Override
+			    public void onCompletion(RecordMetadata recordMetadata,
+			        Exception arg1) {
+				    System.out.println("Record stored successfully!");
+			    }
+		    });
+
+		String msg = null;
+		try {
+			RecordMetadata rm = rs.get();
+			msg = msg + " partition = " + rm.partition() + " offset ="
+			    + rm.offset();
+			System.out.println(msg);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+}
---
+++ src/core/internal/rabbitmq/RabbitMQBroker.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package core.internal.rabbitmq;
+
+import core.internal.AbstractBroker;
+
+/**
+ * @author Saurabh
+ *
+ */
+public class RabbitMQBroker extends AbstractBroker {
+
+	public RabbitMQBroker() {
+	    // TODO Auto-generated constructor stub
+    }
+	
+	@Override
+	public void init() {
+	    // TODO Auto-generated method stub
+	    super.init();
+	}
+	
+	@Override
+	public void close() {
+	    // TODO Auto-generated method stub
+	    super.close();
+	}
+	
+	@Override
+	public void publish(String data, String topic) {
+	    // TODO Auto-generated method stub
+	    super.publish(data, topic);
+	}
+}
